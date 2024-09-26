c8d181ed4b57ee4405e6dcf36daba9d7b6a508a5
Hamdy Emad <hamdyemad274@gmail.com>
2024-09-26 18:42:43 +0200

    Add project files.

---
+++ HamdyApp/Controllers/CategoryController.cs
@@ -0,0 +1,98 @@
+﻿using HamdyApp.Data;
+using HamdyApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HamdyApp.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly HamdyAppDbContext db;
+
+        public CategoryController(HamdyAppDbContext db)
+        {
+            this.db = db;
+        }
+
+        public IActionResult Index()
+        {
+            List<Category> categoriesList = db.Categories.ToList();
+            return View(categoriesList);
+
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            if (category.Name != null && category.Name.ToLower() == "test")
+            {
+                ModelState.AddModelError("", $"Name Cannot be {category.Name}");
+            }
+            if (ModelState.IsValid)
+            {
+                db.Categories.Add(category);
+                db.SaveChanges();
+                TempData["Success"] = "Category Created Successfully!";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = db.Categories.FirstOrDefault(x => x.Id == id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            if (category.Name != null && category.Name.ToLower() == "test")
+            {
+                ModelState.AddModelError("", $"Name Cannot be {category.Name}");
+            }
+            if (ModelState.IsValid)
+            {
+                db.Categories.Update(category);
+                db.SaveChanges();
+                TempData["Success"] = "Category Updated Successfully!";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = db.Categories.FirstOrDefault(x => x.Id == id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Category? categoryFromDb = db.Categories.FirstOrDefault(x => x.Id == id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            db.Categories.Remove(categoryFromDb);
+            db.SaveChanges();
+            TempData["Success"] = "Category Deleted Successfully!";
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ HamdyApp/Data/HamdyAppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using HamdyApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HamdyApp.Data
+{
+    public class HamdyAppDbContext : DbContext
+    {
+        public HamdyAppDbContext(DbContextOptions<HamdyAppDbContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "Horror", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "Sci-Fi", DisplayOrder = 3 }
+           );
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
